f3f7129da9d48f14ced628e291673938806ef9ab
hao.liu <hao.liu@hpe.com>
2017-11-28 18:09:56 +0800

    spring boot get started

---
+++ spring-boot-gs/src/main/java/com/hank/sample/spring/SpringBootGsApplication.java
@@ -0,0 +1,23 @@
+package com.hank.sample.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class SpringBootGsApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(SpringBootGsApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(SpringBootGsApplication.class, args);
+
+		System.out.println("Let's inspect the beans provided by Spring Boot:");
+		String[] beanNames = ctx.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		for (String beanName : beanNames) {
+			System.out.println(beanName);
+		}
+	}
+}
